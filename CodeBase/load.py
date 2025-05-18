1b301b054ef67eae22e0f37532d4ba31df470e19
KalpeshaP <kalpeshanpingale@gmail.com>
2025-05-18 15:53:48 +0530

    New Changes

--- CodeBase/load.py
+++ CodeBase/load.py
@@ -25,7 +25,7 @@ class DataLoading:
     def load_fact_sales_table(self):
         logger.info("Loading for fact_sales started...")
         query = text("""insert into fact_sales(sales_id,product_id,store_id,quantity,total_sales,sale_date)
-                           select sales_id,product_id,store_id,quantity,sales_amount,sale_date from sales_with_details""")
+                        select sales_id,product_id,store_id,quantity,sales_amount,sale_date from sales_with_details""")
         try:
             with mysql_engine.connect() as conn:
                 logger.info("Fact_sales table loading started...")
@@ -36,13 +36,24 @@ class DataLoading:
         except Exception as e:
             logger.error(f"Error while loading fact_sales table {e}", exc_info=True)
 
+    def load_fact_inventory_table(self):
+        logger.info("Loading for Fact_inventory started...")
+        query = text("""insert into fact_inventory(product_id,store_id,quantity_on_hand,last_updated) 
+                        select product_id,store_id,quantity_on_hand,last_updated from staging_inventory""")
+        try:
+            with mysql_engine.connect() as conn:
+                logger.info("Fact_inventory table loading started...")
+                logger.info(query)
+                conn.execute(query)
+                conn.commit()
+                logger.info("Fact_inventory table loading completed...")
+        except Exception as e:
+            logger.error(f"Error while loading Fact_inventory table {e}", exc_info=True)
+
     def load_monthly_sales_summary_table(self):
         logger.info("Loading for monthly_sales_summary started...")
-        query = text("""
-            INSERT INTO monthly_sales_summary(product_id, month, year, total_sales)
-            SELECT product_id, month, year, total_sales
-            FROM monthly_sales_summary_source
-        """)
+        query = text("""insert into monthly_sales_summary(product_id,month,year,total_sales) 
+                        select product_id,month,year,total_sales from monthly_sales_summary_source""")
         try:
             with mysql_engine.connect() as conn:
                 logger.info("monthly_sales_summary table loading started...")
@@ -53,44 +64,24 @@ class DataLoading:
         except Exception as e:
             logger.error(f"Error while loading monthly_sales_summary table {e}", exc_info=True)
 
-    def load_fact_inventory_table(self):
-        logger.info("Loading for fact_inventory started...")
-        query = text("""
-            INSERT INTO fact_inventory(product_id, store_id, quantity_on_hand, last_updated)
-            SELECT product_id, store_id, quantity_on_hand, last_updated
-            FROM staging_inventory
-        """)
+    def load_inventory_level_by_store_table(self):
+        logger.info("Loading for inventory_level_by_store started...")
+        query = text("""insert into inventory_levels_by_store(store_id,total_inventory) 
+                        select store_id,total_inventory from aggregated_inventory_level;""")
         try:
             with mysql_engine.connect() as conn:
-                logger.info("fact_inventory table loading started...")
+                logger.info("inventory_level_by_store table loading started...")
                 logger.info(query)
                 conn.execute(query)
                 conn.commit()
-                logger.info("fact_inventory table loading completed...")
+                logger.info("inventory_level_by_store table loading completed...")
         except Exception as e:
-            logger.error(f"Error while loading fact_inventory table {e}", exc_info=True)
-
-    def load_inventory_levels_by_store_table(self):
-        logger.info("Loading for inventory_levels_by_store started...")
-        query = text("""
-            INSERT INTO inventory_levels_by_store(store_id, total_inventory)
-            SELECT store_id, total_inventory
-            FROM aggregated_inventory_level
-        """)
-        try:
-            with mysql_engine.connect() as conn:
-                logger.info("inventory_levels_by_store table loading started...")
-                logger.info(query)
-                conn.execute(query)
-                conn.commit()
-                logger.info("inventory_levels_by_store table loading completed...")
-        except Exception as e:
-            logger.error(f"Error while loading inventory_levels_by_store table {e}", exc_info=True)
+            logger.error(f"Error while loading inventory_level_by_store table {e}", exc_info=True)
 
 
 if __name__ == "__main__":
     loadRef = DataLoading()
-    #loadRef.load_fact_sales_table()
-    #loadRef.load_monthly_sales_summary_table()
-    #loadRef.load_fact_inventory_table()
-    loadRef.load_inventory_levels_by_store_table()
+    loadRef.load_fact_sales_table()
+    loadRef.load_fact_inventory_table()
+    loadRef.load_monthly_sales_summary_table()
+    loadRef.load_inventory_level_by_store_table()
